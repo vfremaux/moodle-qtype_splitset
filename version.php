b24252e02cf841532efdb8ec029057c150f5a068
valery <valery.fremaux@gmail.com>
2016-10-24 10:31:57 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014121401;
-$plugin->requires = 2015050500;
+$plugin->requires = 2015111000;
 $plugin->component = 'qtype_splitset';
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "2.9.0 (Build 2014121401)";
+$plugin->release = "3.0.0 (Build 2014121401)";
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.9.0000';
+$plugin->codeincrement = '3.0.0000';
