1ddefc1d98a1a68d71af38cece5a5918726d70c4
valery <valery.fremaux@gmail.com>
2017-04-03 11:14:00 +0200

    fix restore and backup / enhance presentation and form

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014121401;
+$plugin->version = 2017040200;
 $plugin->requires = 2016120500;
 $plugin->component = 'qtype_splitset';
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "3.2.0 (Build 2012112300)";
+$plugin->release = "3.2.0 (Build 2017040200)";
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.2.0001';
