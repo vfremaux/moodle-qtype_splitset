e4bcc38efee5f3d5f0af8cc0da95c9bb1cac6c9a
valery <valery.fremaux@gmail.com>
2018-10-08 16:59:12 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2017040200;
-$plugin->requires = 2016051900;
+$plugin->requires = 2018051400;
 $plugin->component = 'qtype_splitset';
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "3.1.0 (Build 2017040200)";
+$plugin->release = "3.5.0 (Build 2017040200)";
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0001';
+$plugin->codeincrement = '3.5.0001';
